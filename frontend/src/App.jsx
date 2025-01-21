5b2d24c56a112976177d6929322814d6f1e97c4f
Andrew Galvan <azhack061@gmail.com>
2025-01-21 16:36:22 -0500

    gestionando el template para la pagina web y sus componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import Workflow from './components/Workflow'
+import Pricing from './components/Pricing'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">   Hello world! </h1>
-    </div>
+    <>
+    <Navbar />
+     <div className="max-w-7xl mx-auto pt-20 px-6">
+     <HeroSection />
+     <FeatureSection />
+      <Workflow />
+      <Pricing />
+     </div>
+    
+    </>
   )
 }
 
---
+++ frontend/src/components/Pricing.jsx
@@ -0,0 +1,8 @@
+import { CheckCircle2 } from "lucide-react";
+const Pricing = () => {
+  return (
+    <div>Pricing</div>
+  )
+}
+
+export default Pricing

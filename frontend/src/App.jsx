5af6f62fbc16d3b33caa5b180ca6273552d9229e
Andrew Galvan <azhack061@gmail.com>
2025-03-18 10:27:15 -0500

    arreglando un poco el desorden antes del routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import FeatureSection from './components/FeatureSection'
-import Workflow from './components/Workflow'
-import Pricing from './components/Pricing'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
 import Home from './components/Home';
 import FooterCT from './components/FooterRsp'
 
@@ -16,24 +10,6 @@ const App = () => {
     <div className='pt-20 px-6 '>
      < Home  /> 
         </div>
- 
-       
-        
-
-     <div className="max-w-7xl mx-auto pt-32 px-6">
-     {/* <HeroSection /> 
-        <FeatureSection />
-      <Workflow />
-      <Pricing />
-     <FeatureSection />
-       <Testimonials />
-        <Footer />
-     */} 
-       
-  
-        </div>
-    
-
         <FooterCT />
      </>
   

f27be3ca0249cc3e4a78b44149f2f4115c1d765d
Andrew Galvan <azhack061@gmail.com>
2025-04-11 09:12:15 -0500

    cambiando un poco el color del navbar mobile bground

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
              </div>
             </div>
             {mobileDrawerOpen && (
-              <div className="fixed right-0 z-20 bg-gray-50 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+              <div className="fixed right-0 z-20 bg-gray-100 w-full p-12 flex flex-col justify-center items-center lg:hidden">
                  <ul>
                    {navItems.map((item, index) => (
                      <li key={index} className="py-4">
